e066cc1f5256766442dd8f78cd6ba99e9867e48a
Kheiria-Hamayel <khiriyya123.amjad@gmail.com>
2024-04-30 15:01:09 +0300

    L#8

--- src/index.mjs
+++ src/index.mjs
@@ -94,6 +94,22 @@ app.patch("/api/users/:id", (req, res) => {
   return res.sendStatus(204);
 });
 
+app.delete("/api/users/:id", (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return res.send(400);
+
+  const findUserIndex = mockUser.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return res.sendStatus(404);
+
+  mockUser.splice(findUserIndex, 1);
+
+  return res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
 });
